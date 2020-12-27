ba471afdf85e4f212c2d5bdc9284fdd10d152a49
Umut Bozdağ <umutbozdag@outlook.com.tr>
2020-12-27 15:53:07 +0300

    delete auth

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const PORT = 3000;
 const bodyParser = require("body-parser");
 const movies = require("./api/routes/movies");
 const search = require("./api/routes/search");
-const auth = require("./api/routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/movies", movies);
 app.use("/search", search);
-app.use("/auth", auth);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
